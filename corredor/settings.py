ea36dfb912539d7229421e5d352240ece7e22b77
unknown <correo.arce@gmail.com>
2025-10-28 19:51:57 -0300

    allowed_host

--- corredor/settings.py
+++ corredor/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'tu_clave_secreta_aqui'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 #ALLOWED_HOSTS = ['192.168.0.163', 'localhost', '127.0.0.1']
 
