54bcf57f0d51dd61af86bdd061f50b68f2f3cbac
miguelarcedev <correo.arce@gmail.com>
2025-11-12 11:06:15 -0300

    se agrego posisiondemo en admin

--- transporte/admin.py
+++ transporte/admin.py
@@ -3,7 +3,7 @@
 # Register your models here.
 
 from django.contrib import admin
-from .models import Usuario, Empresa, Chofer, Vehiculo, Carga, Viaje, PosicionGPS, Alerta, ControlFrontera,RegistroUbicacion
+from .models import Usuario, Empresa, Chofer, Vehiculo, Carga, Viaje, PosicionGPS, Alerta, ControlFrontera,RegistroUbicacion, PosicionDemo
 from django.contrib.auth.models import User
 
 admin.site.register(User)
@@ -16,6 +16,7 @@ admin.site.register(Vehiculo)
 admin.site.register(Carga)
 admin.site.register(Viaje)
 admin.site.register(PosicionGPS)
+admin.site.register(PosicionDemo)
 admin.site.register(Alerta)
 admin.site.register(ControlFrontera)
 admin.site.register(RegistroUbicacion)
