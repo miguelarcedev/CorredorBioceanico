fa7a502f07ed827cca2e1d5251087a7064279b75
miguelarcedev <correo.arce@gmail.com>
2025-11-14 13:32:49 -0300

    se introdujo path

--- transporte/urls.py
+++ transporte/urls.py
@@ -38,10 +38,8 @@ urlpatterns = [
 
 
     # APIs usadas por el mapa
-    path('api/ruta/', views.obtener_ruta, name='obtener_ruta'),
-    path('api/viajes_demo/', views.lista_viajes_demo, name='lista_viajes_demo'),
     path('api/ruta/', views.obtener_ruta, name='api_ruta'),
-    path('api/ruta/', views.obtener_ruta, name='obtener_ruta'),
+    path('api/viajes_demo/', views.lista_viajes_demo, name='lista_viajes_demo'),
 
     path("panel_analitico/", views.panel_analitico, name="panel_analitico"),
 
