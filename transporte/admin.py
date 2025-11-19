9113951686364fbe610044ab8a74aeb0a66fe872
miguelarcedev <correo.arce@gmail.com>
2025-11-19 12:17:09 -0300

    se agrega al admin viaje demo

--- transporte/admin.py
+++ transporte/admin.py
@@ -3,18 +3,17 @@
 # Register your models here.
 
 from django.contrib import admin
-from .models import Usuario, Empresa, Chofer, Vehiculo, Carga, Viaje, PosicionGPS, Alerta, ControlFrontera,RegistroUbicacion, PosicionDemo
+from .models import Usuario, Empresa, Chofer, Vehiculo, Carga, Viaje, ViajeDemo, PosicionGPS, Alerta, ControlFrontera,RegistroUbicacion, PosicionDemo
 from django.contrib.auth.models import User
 
 admin.site.register(User)
-
-
 admin.site.register(Usuario)
 admin.site.register(Empresa)
 admin.site.register(Chofer)
 admin.site.register(Vehiculo)
 admin.site.register(Carga)
 admin.site.register(Viaje)
+admin.site.register(ViajeDemo)
 admin.site.register(PosicionGPS)
 admin.site.register(PosicionDemo)
 admin.site.register(Alerta)
